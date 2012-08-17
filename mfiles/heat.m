56c1f1086a77b67cb1ded9d349bca2e5721e4e5b
Ed Bueler <elbueler@alaska.edu>
2012-08-17 13:17:35 -0800

    edits through end of first stable run of heat.m

--- mfiles/heat.m
+++ mfiles/heat.m
@@ -1,33 +1,36 @@
-function U = heat(D,J,K,dt,N)
-% explicit method for heat equation
-%   u_t = D (u_xx + u_yy)
+function T = heat(D,J,K,dt,N)
+% HEAT  The explicit method for the 2D heat equation
+%   T_t = D (T_xx + T_yy)
 % on -1 < x < 1, -1 < y < 1, for 0 < t < N * dt
-% fixed time steps (compare heatadapt.m)
-% uses specific gaussian initial condition
-% form
-%   U = heat(D,J,K,dt,N)
+% Uses fixed time steps; compare HEATADAPT.  Uses a specific
+% gaussian initial condition.  Usage:
+%   T = heat(D,J,K,dt,N)
 % where
+%   T   = approximate solution at tf = N * dt
 %   D   = diffusivity coeff
-%   J,K = number of points in x,y directions
+%   J,K = number of points in x,y directions, resp.
 %   dt  = fixed time step
-%   N   = number of time steps; final time is tf = N * dt
-% try: heat(1.0,30,30,0.001,0);    % initial condition
-%      heat(1.0,30,30,0.001,20);   % final time 0.02
-%      heat(1.0,30,30,0.004,5);    % final time 0.02; huh?
+%   N   = number of time steps
+% Examples:
+%   >> heat(1.0,30,30,0.001,0);    % just show initial condition
+%   >> heat(1.0,30,30,0.001,20);   % final time 0.02
+%   >> heat(1.0,30,30,0.004,5);    % final time 0.02; huh?
 
-% spatial grid and initial condition:
+% setup spatial grid and initial condition:
 dx = 2 / J;    dy = 2 / K;
 [x,y] = meshgrid(-1:dx:1, -1:dy:1); % (J+1) x (K+1) grid in x,y plane
-U = exp(-30*(x.*x + y.*y));
+T = exp(-30*(x.*x + y.*y));
 fprintf('  doing N = %d steps of dt = %.5f for 0.0 < t < %.3f\n',N,dt,N*dt)
 
-% explicit time steps
-nu_x = dt * D / (dx*dx);    nu_y = dt * D / (dy*dy);
+% do explicit time steps
+nu_x = dt * D / (dx*dx);
+nu_y = dt * D / (dy*dy);
 for n=1:N
+   % colon notation substitutes for double loop over interior grid points
    U(2:J,2:K) = U(2:J,2:K) + ...
        nu_x * ( U(3:J+1,2:K) - 2 * U(2:J,2:K) + U(1:J-1,2:K) ) + ...
        nu_y * ( U(2:J,3:K+1) - 2 * U(2:J,2:K) + U(2:J,1:K-1) );
 end
 
+% show solution
 surf(x,y,U),  shading('interp'),  xlabel x,  ylabel y,  zlabel u
-
