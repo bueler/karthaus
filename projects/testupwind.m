6eb5c69f185079f95c980b770cabcc5d4816470c
Ed Bueler <elbueler@alaska.edu>
2012-09-12 19:24:13 +0200

    tiny bit more on upwind ... need to write it out on papaer

--- projects/testupwind.m
+++ projects/testupwind.m
@@ -7,6 +7,11 @@ M0 = 0.3/spera;
 Hg = 500;  ug = 50/spera;
 [uexact,H0] = exactshelf(x,L,M0,Hg,ug); % exact steady
 u = uexact;  M = M0*ones(size(x));
-H = upwind(x,u,H0,M,10.0*spera);
+xstag = (x(1:J-1) + x(2:J)) / 2.0;
+ustag = (u(1:J-1) + u(2:J)) / 2.0;
+H = upwind(x,ustag,H0,Hg,M,100.0*spera);
 
-plot(x/1000.0,H), xlabel('x  (km)'),  ylabel('thickness  (m)')
+subplot(211)
+plot(x/1000.0,H),  xlabel('x  (km)'),  ylabel('thickness  (m)')
+subplot(212)
+plot(xstag/1000.0,ustag*spera),  xlabel('x  (km)'),  ylabel('velocity  (m/a)')
--- projects/upwind.m
+++ projects/upwind.m
@@ -1,7 +1,10 @@
-function H = upwind(x,u,H0,M,tf,firstorder)
+function H = upwind(x,ustag,H0,Hleft,M,tf,firstorder)
 % UPWIND  Solves
 %    H_t + (uH)_x = M
 % by upwinding, both second-order (default) and first order.
+% Assumes H(x=0) = Hleft given.  Assumes u(x=0) is positive and
+% that u(x(end)) is positive, Dirichlet condition on
+% H is only need at left (x=0) end.
 % form:
 %   FIXME
 % where ...
@@ -9,12 +12,13 @@ function H = upwind(x,u,H0,M,tf,firstorder)
 
 spera = 31556926.0;
 J = length(x);  dx = x(2)-x(1);
-dtCFL = dx / max(abs(u));
+dtCFL = dx / max(abs(ustag));
 N = ceil(tf/dtCFL);  dt = tf/N;
 fprintf('dt for CFL = %.3f a;  doing N = %d steps to final time tf = %.3f a\n',
         dtCFL/spera,N,tf/spera);
 
 H = H0;
 for k = 1:N
-  % FIXME
-end  
+  Hnew(2:J) = H(2:J) + dt * (q(   FIXME
+  H(1) = Hleft;
+end
