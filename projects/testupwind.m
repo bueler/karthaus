2995a0f3036cc5f138ab0dcd87df77583bf07cec
Ed Bueler <elbueler@alaska.edu>
2012-09-13 11:03:02 +0200

    essentially done with setting up upwinding in preparation for project idea 1

--- projects/testupwind.m
+++ projects/testupwind.m
@@ -1,17 +1,44 @@
-% TESTUPWIND  does what you think
+function maxdiff = testupwind(J)
+% TESTUPWIND  Tests the UPWIND code by setting up the van der Veen
+% exact ice shelf solution and then treating it as time dependent.
+% Any changes are a measure of the numerical error in scheme for the mass
+% continuity equation, i.e. in the scheme used in UPWIND.
+% Examples:
+%   >> testupwind           % uses J = 100 subintervals; about 5 sec
+%   >> testupwind(50)       % try coarser
+%   >> testupwind(500)      % try finer; takes a few minutes
+% Calls:  UPWIND, EXACTSHELF
 
+if nargin < 1, J = 100; end
+
+L = 100e3;        % m
+M0pera = 0.3;     % m/a
+Hg = 500.0;       % m
+ugpera = 50.0;    % m/a
+
+x = linspace(0,L,J+1);
 spera = 31556926.0;
-J = 101;  L = 100e3;
-x = linspace(0,L,J);
-M0 = 0.3/spera;
-Hg = 500;  ug = 50/spera;
-[uexact,H0] = exactshelf(x,L,M0,Hg,ug); % exact steady
-u = uexact;  M = M0*ones(size(x));
-xstag = (x(1:J-1) + x(2:J)) / 2.0;
-ustag = (u(1:J-1) + u(2:J)) / 2.0;
-H = upwind(x,ustag,H0,Hg,M,100.0*spera);
+[uexact,Hinitial] = exactshelf(x,L,M0pera/spera,Hg,ugpera/spera); % exact steady
+M = (M0pera/spera) * ones(size(x));
 
-subplot(211)
-plot(x/1000.0,H),  xlabel('x  (km)'),  ylabel('thickness  (m)')
-subplot(212)
-plot(xstag/1000.0,ustag*spera),  xlabel('x  (km)'),  ylabel('velocity  (m/a)')
+% short run
+Hshort  = upwind(x,uexact,Hinitial,Hg,M,  100.0*spera);
+% long run
+Hlong   = upwind(x,uexact,Hinitial,Hg,M,  300.0*spera);
+% longer run
+Hlonger = upwind(x,uexact,Hinitial,Hg,M,30000.0*spera);
+
+figure(1), clf
+subplot(4,1,[1 2])
+plot(x/1000.0,Hinitial,x/1000.0,Hshort,x/1000.0,Hlong,x/1000.0,Hlonger)
+legend('Hinitial','H at 100 a','H at 300 a','H at 30000 a')
+xlabel('x  (km)'),  ylabel('thickness  (m)')
+subplot(4,1,3)
+plot(x/1000.0,(Hshort-Hinitial),x/1000.0,(Hlong-Hinitial),...
+     x/1000.0,(Hlonger-Hinitial))
+legend('at 100 a','at 300 a','at 30000 a')
+xlabel('x  (km)'),  ylabel('thickness difference from initial  (m)')
+subplot(4,1,4),  plot(x/1000.0,uexact*spera)
+xlabel('x  (km)'),  ylabel('velocity  (m/a)')
+
+maxdiff = max([max(abs(Hshort-Hinitial)) max(abs(Hlong-Hinitial)) max(abs(Hlonger-Hinitial))]);
--- projects/upwind.m
+++ projects/upwind.m
@@ -1,24 +1,53 @@
-function H = upwind(x,ustag,H0,Hleft,M,tf,firstorder)
+function H = upwind(x,u,Hinitial,H0,M,tf)
 % UPWIND  Solves
-%    H_t + (uH)_x = M
-% by upwinding, both second-order (default) and first order.
-% Assumes H(x=0) = Hleft given.  Assumes u(x=0) is positive and
-% that u(x(end)) is positive, Dirichlet condition on
-% H is only need at left (x=0) end.
-% form:
-%   FIXME
-% where ...
+%    H_t + (u H)_x = M
+% by conservative first order upwinding (i.e. the donor cell method).
+% Assumes H(x=0) = H0 given.  Assumes u(x=0) is positive, and that
+% u(x(end)) is positive, so that Dirichlet condition on H is only needed
+% at left (x=0) end.
+% Form:
+%   H = upwind(x,u,Hinitial,H0,M,tf)
+% where
+%   H        = output; ice thickness at tf (s) run; J+1 vector (m)
+%   x        = grid; must be equally spaced; J+1 vector (m)
+%   u        = velocity is assumed time-independent; J+1 vector (m/s)
+%   Hinitial = initial ice thickness; J+1 vector (m)
+%   H0       = left hand ice thickness boundary condition, at x(1); scalar (m)
+%   M        = surface mass balance; J+1 vector (m/s)
+%   tf       = duration of run; scalar (s)
+% Note x,u,Hinitial,M,H are all on the "regular" grid even though
+% internally the velocity and flux are computed on the "staggered" grid.
+% Uses the CFL condition to determine the time step.  As text output,
+% one '.' is printed per time step.
 % Example:  TESTUPWIND
 
 spera = 31556926.0;
-J = length(x);  dx = x(2)-x(1);
-dtCFL = dx / max(abs(ustag));
-N = ceil(tf/dtCFL);  dt = tf/N;
+J = length(x)-1;                 % J+1 points  x_1,x_2,...,x_{J+1}
+dx = x(2)-x(1);
+dtCFL = dx / max(abs(u));
+N = ceil(tf/dtCFL);
+dt = tf/N;  % divide into integer # of steps
+nu = dt / dx;
 fprintf('dt for CFL = %.3f a;  doing N = %d steps to final time tf = %.3f a\n',
         dtCFL/spera,N,tf/spera);
 
-H = H0;
+ustag = (u(1:J) + u(2:J+1))/2;
+qstag = zeros(size(ustag));  % merely pre-allocate
+H = Hinitial;
 for k = 1:N
-  Hnew(2:J) = H(2:J) + dt * (q(   FIXME
-  H(1) = Hleft;
+  % compute fluxes at cell boundaries
+  for j = 1:J
+    if ustag(j) >= 0
+      qstag(j) = ustag(j) * H(j);
+    else
+      qstag(j) = ustag(j) * H(j+1);
+    end
+  end
+  % make time-step: update H
+  Hnew(1)   = H0;
+  Hnew(2:J) = H(2:J) + dt * M(2:J) - nu * (qstag(2:J) - qstag(1:J-1));
+  Hnew(J+1) = H(J+1) + dt * M(J+1) - 2 * nu * (u(J+1) * H(J+1) - qstag(J));
+  H = Hnew;  % actually update
+  fprintf('.')
 end
+fprintf('\n')
