07d572294e5f39537d46d6e9c5d795e02d644a0e
Ed Bueler <elbueler@alaska.edu>
2014-09-10 18:49:33 +0200

    Modifications to heatadapt.m were necessary to create a solution to Exercise 8.

--- mfiles/heatadapt.m
+++ mfiles/heatadapt.m
@@ -1,25 +1,28 @@
-function T = heatadapt(D,J,K,tf)
+function T = heatadapt(D,L,J,K,tf,Tinitial)
 % HEATADAPT  Adaptive time-stepping explicit method for heat equation
 %   T_t = D (T_xx + T_yy)
-% on -1 < x < 1, -1 < y < 1, for 0 < t < tf
+% on -L < x < L, -L < y < L, for 0 < t < tf
 % Compare HEAT.  This version uses the same gaussian initial condition.
 % Calling sequence slightly different because time step dt is computed
 % and is not an input.
 % Usage:
-%   T = heatadapt(D,J,K,tf)
+%   T = heatadapt(D,L,J,K,tf,Tinitial)
 % where
 %   T   = approximate solution at tf
 %   D   = diffusivity coeff
+%   L   = solves on square -L < x < L, -L < y < L
 %   J,K = number of subintervals in x,y directions, resp.
 %   tf  = final time
+%   Tinitial = initial state defined on (J+1) x (K+1) grid (optional)
 % Examples:  Compare these
-%   >> heatadapt(1.0,50,50,0.05);
+%   >> heatadapt(1.0,1.0,50,50,0.05);
 %   >> heat(1.0,50,50,0.00025,200);
 
 % spatial grid and initial condition:
-dx = 2 / J;    dy = 2 / K;
-[x,y] = ndgrid(-1:dx:1, -1:dy:1); % (J+1) x (K+1) grid in x,y plane
-T = exp(-30*(x.*x + y.*y));
+dx = 2 * L / J;    dy = 2 * L / K;
+[x,y] = ndgrid(-L:dx:L, -L:dy:L); % (J+1) x (K+1) grid in x,y plane
+if nargin<6, Tinitial = exp(-30*(x.*x + y.*y)); end
+T = Tinitial;
 
 fprintf('  doing explicit steps adaptively on 0.0 < t < %.3f\n',tf)
 t = 0.0;    count = 0;
@@ -38,4 +41,4 @@ while t < tf
 end
 fprintf('\n  completed N = %d steps, average dt = %.7f\n',count,tf/count)
 
-surf(x,y,T),  shading('interp'),  xlabel x,  ylabel y
+figure(1),  surf(x,y,T),  shading('interp'),  xlabel x,  ylabel y
